a361e2c5cb65e3704f9232a5c6767abe8a5ae725
Klaudia <kkborowy@gmail.com>
2024-01-28 18:55:12 +0100

    create a folder

--- file.py
+++ file.py
@@ -1,11 +1,20 @@
 import os
+import shutil
 
 cwd = os.getcwd()
+test_folder_path = cwd + r"\test"
 # path = input('enter path to a folder: ')
-files = os.listdir(cwd)
-
+files = os.listdir(test_folder_path)
+extensions = []
 for file in files:
-    name, extension = os.path.splitext(file)
-    if extension.startswith('.'): extension = extension[1:]
+    names, extension = os.path.splitext(file)
+    extension = extension[1:]
+    # file_path =
+    os.makedirs(test_folder_path + f'/{extension}', exist_ok=True)
+    print(test_folder_path)
 
-# print(dir(os.path))
+# for extension in extensions:
+    # print(extension)
+    # os.makedirs(test_folder_path + f'/{extension}', exist_ok=True)
+
+# print(files)
