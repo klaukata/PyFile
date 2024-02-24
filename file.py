53cf5d7f85ff0b504d3f78d35156df14c5dd058d
Klaudia <kkborowy@gmail.com>
2024-02-24 08:13:19 +0100

    moves both files.ext and folders

--- file.py
+++ file.py
@@ -2,18 +2,15 @@ import os
 import shutil
 
 cwd = os.getcwd()
-test_folder_path = cwd + r"\test"
+test_folder = cwd + r"\test"
 # path = input('enter path to a folder: ') #implement error handling
-files = os.listdir(test_folder_path)
+files = os.listdir(test_folder)
 extensions = []
-for file in files: # folers???
+for file in files:
     names, extension = os.path.splitext(file)
     extension = extension[1:]
-    os.makedirs(test_folder_path + f'/{extension}', exist_ok=True)
-    # print(f'{test_folder_path}\{file}')
-    shutil.move(f'{test_folder_path}\{file}', f'{test_folder_path}\{extension}\{file}')
-# for extension in extensions:
-    # print(extension)
-    # os.makedirs(test_folder_path + f'/{extension}', exist_ok=True)
-
-# print(files)
+    folder_path = test_folder + f'/{extension}'
+    folder_path_for_folders = f'{test_folder}/folders'
+    proper_folder_path = folder_path_for_folders if extension == "" else folder_path
+    os.makedirs(proper_folder_path, exist_ok=True)
+    shutil.move(f'{test_folder}\{file}', f'{proper_folder_path}\{file}')
