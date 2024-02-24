b72821c4c70ee60b080936b64c9f67f7f5b9631f
Klaudia <kkborowy@gmail.com>
2024-02-24 08:41:16 +0100

    Error handling for a path from a user

--- file.py
+++ file.py
@@ -1,16 +1,24 @@
 import os
 import shutil
 
-cwd = os.getcwd()
-test_folder = cwd + r"\test"
-# path = input('enter path to a folder: ') #implement error handling
-files = os.listdir(test_folder)
-extensions = []
-for file in files:
-    names, extension = os.path.splitext(file)
-    extension = extension[1:]
-    folder_path = test_folder + f'/{extension}'
-    folder_path_for_folders = f'{test_folder}/folders'
-    proper_folder_path = folder_path_for_folders if extension == "" else folder_path
-    os.makedirs(proper_folder_path, exist_ok=True)
-    shutil.move(f'{test_folder}\{file}', f'{proper_folder_path}\{file}')
+def get_path():
+    user_path = input('enter an absolute path to a folder that you want to organize: ')
+    try:
+        files_arr = os.listdir(user_path)
+        organize(files_arr, user_path)
+        print('Done!')
+    except OSError:
+        print('Incorrect folder path. Try again')
+        get_path()
+
+def organize(files, path):
+    for file in files:
+        names, extension = os.path.splitext(file)
+        extension = extension[1:]
+        folder_path = path + f'/{extension}'
+        folder_path_for_folders = f'{path}/folders'
+        proper_folder_path = folder_path_for_folders if extension == "" else folder_path
+        os.makedirs(proper_folder_path, exist_ok=True)
+        shutil.move(f'{path}\{file}', f'{proper_folder_path}\{file}')
+
+get_path()
