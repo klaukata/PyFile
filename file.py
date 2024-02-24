e27589ac7a779c6c48fdbee874a2bf1b0c5da37d
Klaudia <kkborowy@gmail.com>
2024-02-24 07:55:37 +0100

    moves files to its app. folders

--- file.py
+++ file.py
@@ -3,16 +3,15 @@ import shutil
 
 cwd = os.getcwd()
 test_folder_path = cwd + r"\test"
-# path = input('enter path to a folder: ')
+# path = input('enter path to a folder: ') #implement error handling
 files = os.listdir(test_folder_path)
 extensions = []
-for file in files:
+for file in files: # folers???
     names, extension = os.path.splitext(file)
     extension = extension[1:]
-    # file_path =
     os.makedirs(test_folder_path + f'/{extension}', exist_ok=True)
-    print(test_folder_path)
-
+    # print(f'{test_folder_path}\{file}')
+    shutil.move(f'{test_folder_path}\{file}', f'{test_folder_path}\{extension}\{file}')
 # for extension in extensions:
     # print(extension)
     # os.makedirs(test_folder_path + f'/{extension}', exist_ok=True)
